70d38ff2205d498ab436f88f8ebdd8c4f32fd823
Bogdan <boggzon@gmail.com>
2024-05-21 19:39:08 +0300

    worktime calculation

--- mpi_test.cpp
+++ mpi_test.cpp
@@ -6,12 +6,12 @@ double calculatePi(long long num_samples, unsigned int seed) {
     long long num_points_in_circle = 0;
 
     // Для рандома
-    std::mt19937 gen(seed); 
-    std::uniform_real_distribution<double> dis(0.0, 1.0); 
+    std::mt19937 gen(seed);
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
 
     for (long long i = 0; i < num_samples; i++) {
-        double x = dis(gen); 
-        double y = dis(gen); 
+        double x = dis(gen);
+        double y = dis(gen);
         double distance_squared = x * x + y * y;
         if (distance_squared <= 1) {
             num_points_in_circle++;
@@ -33,17 +33,22 @@ int main(int argc, char** argv) {
 
     unsigned int seed = time(NULL) + rank; // Генерация seed для каждого процесса
 
+    double start_time = MPI_Wtime();
     double local_pi = calculatePi(samples_per_process, seed);
+    double end_time = MPI_Wtime();
+    double local_elapsed_time = end_time - start_time;
 
     double global_pi;
+    double global_elapsed_time;
+
     MPI_Reduce(&local_pi, &global_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_elapsed_time, &global_elapsed_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         global_pi /= size;
-        std::cout << "ПИ: " << global_pi << std::endl;
+        std::cout << "ПИ: " << global_pi << "   Время выполнения: " << global_elapsed_time << " секунд" << std::endl;
     }
 
     MPI_Finalize();
     return 0;
 }
-
